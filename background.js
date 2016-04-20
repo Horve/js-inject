2887728e78424e8c7c75e03b8d477fdd2a16a23d
shun.zheng <shun.zheng@corp.elong.com>
2016-04-20 11:40:01 +0800

    更新业务

--- background.js
+++ background.js
@@ -3,26 +3,25 @@ chrome.webRequest.onBeforeRequest.addListener(
   function(details) {
     console.log(details);
     // 弹出通知
-    chrome.notifications.clear("newNotice", function( wasClear ) {
-  		chrome.notifications.create("newNotice", {
-				type: "basic",
-				iconUrl: chrome.runtime.getURL("images/logo.png"),
-				title: "页面JS拦截提醒",
-				message: "拓展将开启页面JS拦截，若要恢复js执行请关闭拓展。"
-			}, function( notificationId ) {
-				console.log(notificationId);
-			} );
-    });
-    return {redirectUrl: chrome.extension.getURL("returnjs.js")};
-  },
-  {
-    urls: [
-    	"*://*.jquery.top/*",
-      "*://*.elongstatic.com/*",
-      "*://*.51vv.com/*"
-    ],
-    types: ["script"]
-  },
-  ["blocking"]
+    chrome.notifications.clear("newNotice",
+        function(wasClear) {
+            chrome.notifications.create("newNotice", {
+                type: "basic",
+                iconUrl: chrome.runtime.getURL("images/logo.png"),
+                title: "页面JS拦截提醒",
+                message: "拓展将开启页面JS拦截，若要恢复js执行请关闭拓展。"
+            },
+            function(notificationId) {
+                console.log(notificationId);
+            });
+        });
+        return {
+            redirectUrl: chrome.extension.getURL("returnjs.js")
+        };
+    },
+    {
+        urls: ["*://*.jquery.top/*", "*://*.elongstatic.com/*", "*://*.51vv.com/*"],
+        types: ["script"]
+    },
+    ["blocking"]
 );
-
